73c0990601f1cb6cdf2e1cd231ef9438e669f486
DrorTsky <drortartak@gmail.com>
2023-02-14 12:14:05 +0200

    adds imports

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import {Home} from './routes/Home'
 import './App.css';
+import {Home} from './routes/Home'
+import { Routes, Route} from "react-router-dom";
+import Navbar from './components/Navbar';
 
 function App() {
   return (
