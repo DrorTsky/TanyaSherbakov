80702b9acfeedc95da4288096b8dce86c2af8657
DrorTsky <drortartak@gmail.com>
2023-04-22 18:53:48 +0300

    updates images to webp

--- src/components/SvgStyles.js
+++ src/components/SvgStyles.js
@@ -1,109 +1,61 @@
 /** @format */
 
-import styled, { css } from "styled-components";
-// import MarketLogWEBP from "../images/marketlog.webp";
-// import IchiWEBP from "../images/ichi.webp";
-// import KillbillsWEBP from "../images/killbills.webp";
-// import MaynoothWEBP from "../images/maynooth.webp";
-// import BlueBlobWEBP from "../images/blueBlob.webp";
-// import PinkBlobWEBP from "../images/pinkBlob.webp";
-// import DoubleBlobWEBP from "../images/doubleBlob.webp";
-// import IsraelWEBP from "../images/israel.webp";
-// import ArrowWEBP from "../images/arrow.webp";
-// import AboutWEBP from "../images/about_wildheart.webp";
-// import PatzankiOneWEBP from "../images/Patzanki 1.webp";
-// import PatzankiTwoWEBP from "../images/Patzanki 2.webp";
-// import PatzankiThreeWEBP from "../images/Patzanki 3.webp";
-// import PatzankiFourWEBP from "../images/Patzanki 4.webp";
-// import PatzankiFiveWEBP from "../images/Patzanki 5.webp";
-// import PatzankiSixWEBP from "../images/Patzanki 6.webp";
-// import PatzankiSevenWEBP from "../images/Patzanki 7.webp";
-// import PatzankiEightWEBP from "../images/Patzanki 8.webp";
-// import MalluableOneWEBP from "../images/Malluable 1.webp";
-// import MalluableTwoWEBP from "../images/Malluable 2.webp";
-// import MalluableThreeWEBP from "../images/Malluable 3.webp";
-// import MagazinesOneWEBP from "../images/Magazines.webp";
-// import MagazinesTwoWEBP from "../images/Magazines 2.webp";
-// import AfterdarkWEBP from "../images/Afterdark.webp";
-// import MarketLogOverviewWEBP from "../images/marketlog-overview.webp";
-// import MarketLogUnderstandingWEBP from "../images/marketlog-understanding.webp";
-// // import { ReactComponent as MarketLogWEBP } from "../images/marketlog.webp";
-// // import { ReactComponent as IchiWEBP } from "../images/ichi.webp";
-// // import { ReactComponent as KillbillsWEBP } from "../images/killbills.webp";
-// // import { ReactComponent as MaynoothWEBP } from "../images/maynooth.webp";
-// // import { ReactComponent as BlueBlobWEBP } from "../images/blueBlob.webp";
-// // import { ReactComponent as PinkBlobWEBP } from "../images/pinkBlob.webp";
-// // import { ReactComponent as DoubleBlobWEBP } from "../images/doubleBlob.webp";
-// // import { ReactComponent as IsraelWEBP } from "../images/israel.webp";
-// // import { ReactComponent as ArrowWEBP } from "../images/arrow.webp";
-// // import { ReactComponent as AboutWEBP } from "../images/about_wildheart.webp";
-// // import { ReactComponent as PatzankiOneWEBP } from "../images/Patzanki 1.webp";
-// // import { ReactComponent as PatzankiTwoWEBP } from "../images/Patzanki 2.webp";
-// // import { ReactComponent as PatzankiThreeWEBP } from "../images/Patzanki 3.webp";
-// // import { ReactComponent as PatzankiFourWEBP } from "../images/Patzanki 4.webp";
-// // import { ReactComponent as PatzankiFiveWEBP } from "../images/Patzanki 5.webp";
-// // import { ReactComponent as PatzankiSixWEBP } from "../images/Patzanki 6.webp";
-// // import { ReactComponent as PatzankiSevenWEBP } from "../images/Patzanki 7.webp";
-// // import { ReactComponent as PatzankiEightWEBP } from "../images/Patzanki 8.webp";
-// // import { ReactComponent as MalluableOneWEBP } from "../images/Malluable 1.webp";
-// // import { ReactComponent as MalluableTwoWEBP } from "../images/Malluable 2.webp";
-// // import { ReactComponent as MalluableThreeWEBP } from "../images/Malluable 3.webp";
-// // import { ReactComponent as MagazinesOneWEBP } from "../images/Magazines.webp";
-// // import { ReactComponent as MagazinesTwoWEBP } from "../images/Magazines 2.webp";
-// // import { ReactComponent as AfterdarkWEBP } from "../images/Afterdark.webp";
-// // import { ReactComponent as MarketLogOverviewWEBP } from "../images/marketlog-overview.webp";
-// // import { ReactComponent as MarketLogUnderstandingWEBP } from "../images/marketlog-understanding.webp";
+import styled from "styled-components";
 
-// export const MarketLogOverviewWEBPStyle = styled(MarketLogOverviewWEBP)`
-//   // height: 905px;
-//   // width: 490px;
-//   width: 50%;
+import { ReactComponent as BlueBlobSVG } from "../images/blueBlob.svg";
+import { ReactComponent as PinkBlobSVG } from "../images/pinkBlob.svg";
+import { ReactComponent as ArrowSVG } from "../images/arrow.svg";
 
-//   @media (max-width: 768px) {
-//     width: 330px;
-//     heightr: 610px;
-//   }
-// `;
+export const OverviewStyledImage = styled.img`
+  // height: 905px;
+  // width: 490px;
+  width: 50%;
 
-// export const MarketLogUnderstandingWEBPStyle = styled(
-//   MarketLogUnderstandingWEBP
-// )`
-//   height: 641px;
-//   width: 493px;
+  @media (max-width: 768px) {
+    width: 330px;
+    height: 610px;
+    align-self: center;
+  }
+`;
 
-//   @media (max-width: 768px) {
-//     width: 330px;
-//     heightr: 430px;
-//   }
-// `;
-// export const BlueBlobWEBPStyle = styled(BlueBlobWEBP)`
-//   margin-top: 5px;
-//   align-self: center;
-//   @media (max-width: 768px) {
-//     height: 26px;
-//     width: 25px;
-//   }
-// `;
+export const UnderstandingStyledImage = styled.img`
+  height: 641px;
+  width: 493px;
 
-// export const HeaderBlueBlobWEBPStyle = styled(BlueBlobWEBP)`
-//   height: 32px;
-//   width: 31px;
-//   margin-top: 5px;
-//   align-self: center;
-//   @media (max-width: 768px) {
-//     height: 24px;
-//     width: 23px;
-//   }
-// `;
+  @media (max-width: 768px) {
+    width: 330px;
+    heightr: 430px;
+  }
+`;
 
-// export const PinkBlobWEBPStyle = styled(PinkBlobWEBP)`
-//   margin-top: 5px;
-//   align-self: center;
-//   @media (max-width: 768px) {
-//     height: 26px;
-//     width: 25px;
-//   }
-// `;
+export const BlueBlobSVGStyle = styled(BlueBlobSVG)`
+  margin-top: 5px;
+  align-self: center;
+  @media (max-width: 768px) {
+    height: 26px;
+    width: 25px;
+  }
+`;
+
+export const HeaderBlueBlobSVGStyle = styled(BlueBlobSVG)`
+  height: 32px;
+  width: 31px;
+  margin-top: 5px;
+  align-self: center;
+  @media (max-width: 768px) {
+    height: 24px;
+    width: 23px;
+  }
+`;
+
+export const PinkBlobSVGStyle = styled(PinkBlobSVG)`
+  margin-top: 5px;
+  align-self: center;
+  @media (max-width: 768px) {
+    height: 26px;
+    width: 25px;
+  }
+`;
 
 // export const MarketLogWEBPStyle = styled(MarketLogWEBP)`
 //   pointer-events: none;
@@ -152,15 +104,15 @@ import styled, { css } from "styled-components";
 //   }
 // `;
 
-// export const ArrowWEBPStyle = styled(ArrowWEBP)`
-//   width: 36px;
-//   height: 46px;
-//   path {
-//     ${(props) => `fill: ${props.color}`}
-//   }
-//   @media (max-width: 768px) {
-//   }
-// `;
+export const ArrowSVGStyle = styled(ArrowSVG)`
+  width: 36px;
+  height: 46px;
+  path {
+    ${(props) => `fill: ${props.color}`}
+  }
+  @media (max-width: 768px) {
+  }
+`;
 
 // export const AboutWEBPStyle = styled(AboutWEBP)`
 //   width: 608px;
@@ -171,115 +123,62 @@ import styled, { css } from "styled-components";
 //   }
 // `;
 
-// const BigPatzanki = css`
-//   width: 525.27px;
-//   height: 743px;
-//   @media (max-width: 768px) {
-//     width: 330px;
-//     height: 466.79px;
-//   }
-// `;
-
-// const SmallPatzanki = css`
-//   width: 525.04px;
-//   height: 317px;
-//   @media (max-width: 768px) {
-//     width: 330px;
-//     height: 199.21px;
-//   }
-// `;
-
-// export const PatzankiOneWEBPStyle = styled(PatzankiOneWEBP)`
-//   ${BigPatzanki}
-// `;
-
-// export const PatzankiTwoWEBPStyle = styled(PatzankiTwoWEBP)`
-//   ${BigPatzanki}
-// `;
-
-// export const PatzankiSevenWEBPStyle = styled(PatzankiSevenWEBP)`
-//   ${BigPatzanki}
-// `;
-
-// export const PatzankiEightWEBPStyle = styled(PatzankiEightWEBP)`
-//   ${BigPatzanki}
-// `;
-
-// export const PatzankiThreeWEBPStyle = styled(PatzankiThreeWEBP)`
-//   ${SmallPatzanki}
-// `;
-
-// export const PatzankiFourWEBPStyle = styled(PatzankiFourWEBP)`
-//   ${SmallPatzanki}
-// `;
-
-// export const PatzankiFiveWEBPStyle = styled(PatzankiFiveWEBP)`
-//   ${SmallPatzanki}
-// `;
-
-// export const PatzankiSixWEBPStyle = styled(PatzankiSixWEBP)`
-//   ${SmallPatzanki}
-// `;
-
-// export const MalluableOneWEBPStyle = styled(MalluableOneWEBP)`
-//   width: 696px;
-//   height: 522px;
-//   @media (max-width: 768px) {
-//     width: 330px;
-//     height: 247.5px;
-//   }
-// `;
-
-// export const MalluableTwoWEBPStyle = styled(MalluableTwoWEBP)`
-//   width: 696px;
-//   height: 522px;
-//   @media (max-width: 768px) {
-//     width: 330px;
-//     height: 247.5px;
-//   }
-// `;
-
-// export const MalluableThreeWEBPStyle = styled(MalluableThreeWEBP)`
-//   width: 1447px;
-//   height: 1085.25px;
-//   @media (max-width: 768px) {
-//     width: 330px;
-//     height: 247.5px;
-//   }
-// `;
-
-// export const MagazinesOneWEBPStyle = styled(MagazinesOneWEBP)`
-//   width: 696px;
-//   height: 522px;
-//   @media (max-width: 768px) {
-//     width: 330px;
-//     height: 247.5px;
-//   }
-// `;
-
-// export const MagazinesTwoWEBPStyle = styled(MagazinesTwoWEBP)`
-//   width: 696px;
-//   height: 522px;
-//   @media (max-width: 768px) {
-//     width: 330px;
-//     height: 247.5px;
-//   }
-// `;
-
-// export const AfterdarkWEBPStyle = styled(AfterdarkWEBP)`
-//   height: 1087px;
-//   width: 1449px;
-
-//   @media (max-width: 768px) {
-//     width: 330px;
-//     height: 247.5px;
-//   }
-// `;
-
-export const StyledImage = styled.img`
-  pointer-events: none;
+export const MagazinesStyledImage = styled.img`
+  width: 696px;
+  height: 522px;
   @media (max-width: 768px) {
-    height: 40%;
+    width: 330px;
+    height: 247.5px;
+  }
+`;
+
+export const AfterdarkStyledImage = styled.img`
+  height: 1087px;
+  width: 1449px;
+
+  @media (max-width: 768px) {
+    width: 330px;
+    height: 247.5px;
+  }
+`;
+
+export const MarketlogPreviewStyledImage = styled.img`
+  pointer-events: none;
+  width: 1136px;
+  height: 618px;
+  @media (max-width: 768px) {
+    width: 327.71px;
+    height: 178.28px;
+  }
+`;
+
+export const KillbillsPreviewStyledImage = styled.img`
+  pointer-events: none;
+  width: 1359.19px;
+  height: 618px;
+  @media (max-width: 768px) {
+    width: 330px;
+    height: 149px;
+  }
+`;
+
+export const MaynoothPreviewStyledImage = styled.img`
+  pointer-events: none;
+  width: 1275px;
+  height: 809px;
+  @media (max-width: 768px) {
+    width: 323px;
+    height: 205px;
+  }
+`;
+
+export const IchiPreviewStyledImage = styled.img`
+  pointer-events: none;
+  width: 930px;
+  height: 839px;
+  @media (max-width: 768px) {
+    width: 330px;
+    height: 298px;
   }
 `;
 
@@ -299,5 +198,68 @@ export const IsraelStyledImage = styled.img`
   @media (max-width: 768px) {
     width: 9px;
     height: 31px;
+  }
+`;
+
+export const AboutStyledImage = styled.img`
+  width: 608px;
+  height: 606px;
+  @media (max-width: 768px) {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+export const BigPatzankiStyledImage = styled.img`
+  width: 525.27px;
+  height: 743px;
+  @media (max-width: 768px) {
+    width: 330px;
+    height: 466.79px;
+  }
+`;
+
+export const SmallPatzankiStyledImage = styled.img`
+  width: 525.04px;
+  height: 317px;
+  @media (max-width: 768px) {
+    width: 330px;
+    height: 199.21px;
+  }
+`;
+
+export const MalluableSmallStyledImage = styled.img`
+  width: 696px;
+  height: 522px;
+  @media (max-width: 768px) {
+    width: 330px;
+    height: 247.5px;
+  }
+`;
+
+export const MalluableBigStyledImage = styled.img`
+  width: 1447px;
+  height: 1085.25px;
+  @media (max-width: 768px) {
+    width: 330px;
+    height: 247.5px;
+  }
+`;
+
+export const MarketlogResultBigStyledImage = styled.img`
+  width: 862px;
+  height: 522px;
+  @media (max-width: 768px) {
+    width: 330px;
+    height: 200px;
+  }
+`;
+
+export const MarketlogResultSmallStyledImage = styled.img`
+  width: 282px;
+  height: 522px;
+  @media (max-width: 768px) {
+    width: 108px;
+    height: 200px;
   }
 `;
