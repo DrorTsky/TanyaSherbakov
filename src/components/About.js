b70dec95e6702f1abaaa523e02b64676742ca969
DrorTsky <drortartak@gmail.com>
2024-04-08 21:03:44 +0300

    adds hobbie

--- src/components/About.js
+++ src/components/About.js
@@ -112,16 +112,17 @@ const About = () => {
       <InterestsContainer>
         <LearningConainer>
           <BoldedLearningText>What I like</BoldedLearningText>
-          <RegularText>Illustration</RegularText>
+          <RegularText>🖋️ Illustration</RegularText>
           <RegularText>Figma</RegularText>
-          <RegularText>Cats (I am allergic though..)</RegularText>
-          <RegularText>Video games</RegularText>
+          <RegularText>🐈 Cats (I am allergic though..)</RegularText>
+          <RegularText>👾 Video games</RegularText>
         </LearningConainer>
         <LearningConainer>
           <BoldedLearningText>What I'm learning</BoldedLearningText>
           <RegularText>Procreate</RegularText>
-          <RegularText>Copyright & Storytelling</RegularText>
+          <RegularText>✍🏻 Copywriting & Storytelling</RegularText>
           <RegularText>Branding</RegularText>
+          <RegularText>🎤 Singing</RegularText>
         </LearningConainer>
       </InterestsContainer>
     </Container>
