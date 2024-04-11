e1d7b1ebe9a2d405ab2ad3f349cc7a50821ca5f2
DrorTsky <drortartak@gmail.com>
2024-04-11 22:23:20 +0300

    fixes

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -90,7 +90,6 @@ export const Home = ({ isScrollToId }) => {
 
   useEffect(() => {
     const element = document.getElementById("marketlog");
-    console.log(isScrollToId);
     if (element && isScrollToId) {
       element.scrollIntoView({
         behavior: "smooth",
